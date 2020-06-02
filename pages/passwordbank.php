57a5d7833a912d0044e8dc0d9b4024a77b28099b
VitorgsRuffo <vitorruffo19cc@gmail.com>
2020-06-02 17:15:13 -0300

    Finishing Galaxy Defenders page + Creating other projects'page.

--- pages/passwordbank.php
+++ pages/passwordbank.php
@@ -1,15 +1,14 @@
 <?php require("../templates/header.php"); ?>
 <main>
     <div class="wrapper">
-        <h2>Galaxy Defenders</h2>
+        <h2>PasswordBank</h2>
         <section class="gallery">
-                <img src="../images/gd-menu.jpg" alt="galaxy defenders menu">
+                <img src="../images/pb-home.png" alt="PasswordBank home page">
         </section>
         <section class="info">
             <article class="description">  
                 <h3>Description</h3>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis libero, nobis soluta eaque voluptatum doloribus labore exercitationem! Molestiae deserunt a laborum veritatis corporis quis doloribus, sed iste qui fugit cum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur recusandae expedita sit officia obcaecati tempora doloribus, dolor ducimus odio quisquam harum commodi modi atque fugiat ipsum maxime saepe amet libero? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas doloribus obcaecati id aliquam aut! Veritatis doloremque odio, error debitis quidem itaque non ipsum quam assumenda unde deserunt inventore quasi. Eligendi.</p>
-                <!--see how a can put the .rar(ubuntu and windows) of the game so that people can download it. -->
             </article>
             <div class="contact">
                 <p>Contact me</p>
