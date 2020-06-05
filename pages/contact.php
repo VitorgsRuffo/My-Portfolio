c54f7eae4b0c5d897bbbf232038f17a2e96f1d43
VitorgsRuffo <vitorruffo19cc@gmail.com>
2020-06-05 16:47:58 -0300

    Building contact form script to send emails
    library used: PHPMailer

--- pages/contact.php
+++ pages/contact.php
@@ -3,18 +3,24 @@
     <div class="wrapper">
         <h2>Contact</h2>
         <section class="contact-area">
-            <p>Do you have something to ask me? Send me a massage! Here you can also report bugs or just give me some feedback.</p>
+            <?php if(isset($_GET['success']) && $_GET['success'] === 'emailsent'): ?>
+                <p class="emailSent">Your email was sent.<br>Thank you for your time.</p>
+            <?php elseif(isset($_GET['error']) && $_GET['error'] === 'emailnotsent'): ?>
+                <p class="emailNotSent">It was not possible to send your email due to some error in the server. You can also contact me at vitor.gs.ruffo@gmail.com.</p>
+            <?php else: ?>
+                <p>Do you have something to ask me? Send me an e-mail! Here you can also report bugs or just give me some feedback.</p>
+            <?php endif; ?>
             <form action="../scripts/contact-script.php" method="POST" class="form">
                 <div class="inputs">
-                    <input type="text" name="name" placeholder="Enter your name">
-                    <input type="text" name="email" placeholder="Enter your e-mail address">
+                    <input type="text" name="name" placeholder="Your name">
+                    <input type="text" name="email" placeholder="Your e-mail address">
                     <input type="text" name="subject" placeholder="Subject">
-                    <textarea name="massage" placeholder="Enter your massage.."></textarea>
+                    <textarea name="message" placeholder="Enter your message.."></textarea>
                 </div>
                 <div class="sendButton">
                     <input type="submit" name="submit" value="Send">
                 </div>
-            </form>
+            </form> <!-- validate data in the client part with JS -->
             <div class="contact">
                 <p>Contact me</p>
                 <p>E-mail: vitor.gs.ruffo@gmail.com</p>
--- scripts/contact-script.php
+++ scripts/contact-script.php
@@ -1,7 +1,55 @@
 <?php
+    if(isset($_POST['submit'])){
+        //get message data:
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
 
+        //send me an email:
+        require_once("../lib/PHPMailer/PHPMailerAutoload.php");
 
+        $mail = new PHPMailer;
 
+        //Enable SMTP debugging. 
+        $mail->SMTPDebug = 1;
+        //Set PHPMailer to use SMTP.
+        $mail->isSMTP();
+        $mail->Mailer = "smtp";
+        //Set SMTP host name    
+        $mail->Host = 'smtp.gmail.com';
+        //Set this to true if SMTP host requires authentication to send email(Gmail requires an authentication)
+        $mail->SMTPAuth = true;
+        //username and password for the authentication
+        $mail->Username = "";
+        $mail->Password = "";
+        //If SMTP requires TLS encryption then set it(gmail requires it!)
+        $mail->SMTPSecure = 'tls';
+        //Set TCP port to connect to 
+        $mail->Port = 587; 
 
+        //who is sending the email
+        $mail->SetFrom($email, $name);
 
+        //who will receive the email.
+        $mail->AddAddress("vitor.gs.ruffo@gmail.com");
+
+        //who will receive the reply-email
+        $mail->addReplyTo($email);
+
+        $mail->isHTML();
+        $mail->Subject = $subject;
+        $mail->Body = $message."\n<p>This e-mail was sent by ".$name." : ".$email.".</p>";
+
+        if(!$mail->Send()){
+            header("Location: ../pages/contact.php?error=emailnotsent&info=".$mail->ErrorInfo);
+            exit();
+        }else{
+            header("Location: ../pages/contact.php?success=emailsent");
+            exit();
+        }
+            
+    }else{
+        header("Location: ../index.php");
+    }
 ?>
